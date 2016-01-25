0c1f469dc21d6ccfbe0de066043a050cce0a9da4
guohang.bao <guohang.bao@langtaojin.com>
2016-01-25 11:57:18 +0800

    forum post register subject

--- mypipe-kafka/src/main/scala/mypipe/producer/SchemaRepos.scala
+++ mypipe-kafka/src/main/scala/mypipe/producer/SchemaRepos.scala
@@ -2,7 +2,7 @@ package mypipe.producer
 
 import mypipe.api.event.Mutation
 import mypipe.avro.schema.{AvroSchema, AvroSchemaUtils, ShortSchemaId}
-import mypipe.avro.{ForumPostDelete, ForumPostInsert, ForumPostUpdate, InMemorySchemaRepo}
+import mypipe.avro._
 import org.apache.avro.Schema
 
 /**
@@ -10,8 +10,11 @@ import org.apache.avro.Schema
   */
 object ForumPostSchemaRepo extends InMemorySchemaRepo[Short, Schema] with ShortSchemaId with AvroSchema {
   val DATABASE = "medusa"
-  val TABLE = "forum_post"
-  val insertSchemaId = registerSchema(AvroSchemaUtils.specificSubject(DATABASE, TABLE, Mutation.InsertString), new ForumPostInsert().getSchema)
-  val updateSchemaId = registerSchema(AvroSchemaUtils.specificSubject(DATABASE, TABLE, Mutation.UpdateString), new ForumPostUpdate().getSchema)
-  val deleteSchemaId = registerSchema(AvroSchemaUtils.specificSubject(DATABASE, TABLE, Mutation.DeleteString), new ForumPostDelete().getSchema)
+
+  for (i <- 0 until 20) {
+    registerSchema(AvroSchemaUtils.specificSubject(DATABASE, s"forum_post_p$i", Mutation.InsertString), new ForumPostInsert().getSchema)
+    registerSchema(AvroSchemaUtils.specificSubject(DATABASE, s"forum_post_p$i", Mutation.UpdateString), new ForumPostUpdate().getSchema)
+    registerSchema(AvroSchemaUtils.specificSubject(DATABASE, s"forum_post_p$i", Mutation.DeleteString), new ForumPostDelete().getSchema)
+  }
+  
 }
